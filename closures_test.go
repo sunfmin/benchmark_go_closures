230d135c2fb8753136e08e87bb3646b0d3ed9f23
Felix Sun <sunfmin@gmail.com>
2012-09-19 10:29:58 +0800

    add reusing benchmark anonymous funcs

--- closures_test.go
+++ closures_test.go
@@ -18,6 +18,20 @@ func BenchmarkAnonymousFunction(b *testing.B) {
 	fmt.Sprintln(t)
 }
 
+func BenchmarkReusingAnonymousFunction(b *testing.B) {
+	var t int64
+	f := func(j int64) (r int64) {
+		r = j * 2
+		return
+	}
+
+	for i := 0; i < b.N; i++ {
+		t = f(int64(i))
+	}
+
+	fmt.Sprintln(t)
+}
+
 func BenchmarkClosure(b *testing.B) {
 	var t int64
 	for i := 0; i < b.N; i++ {
